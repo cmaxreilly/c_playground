393cf140f8eda13ff0fbc0a15ebd3fe5093f0c3e
cmaxreilly <c.maxwell.reilly@gmail.com>
2022-10-18 19:46:54 -0600

    Messing with string concantation in c

---
+++ string_concantation.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char destination[] = "Hello ";
+	char source[] = "World!";
+	strcat(destination,source);
+	printf("Concatenated String: %s\n", destination);
+	return 0;
+}
