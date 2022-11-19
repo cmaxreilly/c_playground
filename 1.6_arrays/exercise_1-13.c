f854391031833ce64d4f7b28a110babbf1050e6f
cmaxreilly <c.maxwell.reilly@gmail.com>
2022-11-19 12:55:10 -0700

    Made another excercise in section 1.6 of chapter 1

---
+++ 1.6_arrays/exercise_1-13.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/* Assignment: Write a program to print a histogram of the lengths of words in its input.
+ * It is easy to draw the histogram with the bars horizontal; a vertical orientation is more challenging */
+int main(){
+	/* Initialize an integer counter for things?
+	 * iterator for list comprehension and final print statement
+	 * number of white space characters
+	 * number of other characters */
+	int c, i, nwhite, nother;
+	/* Initialize ten digit array */
+	int ndigit[10];
+
+	/* Set number of white and number of other characters to zero */
+
+	nwhite = nother = 0;
+
+	/* Set number of each digit to zero */
+
+	for (i = 0; i < 10; ++i)
+		ndigit[i] = 0;
+
+	while ((c = getchar()) != EOF)
+		if (c >= '0' && c <= '9')
+			++ndigit[c-'0'];
+		else if (c == ' ' || c == '\n' || c == '\t')
+			++nwhite;
+		else
+			++nother;
+	printf("\ndigits =");
+	for (i = 0; i < 10; ++i)
+		printf(" %d", ndigit[i]);
+	printf(", white space = %d, other = %d\n", nwhite, nother);
+}
+
+
