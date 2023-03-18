f7bad92955bba33bd2f0353a4f77dae63144d2c6
cmaxreilly <c.maxwell.reilly@gmail.com>
2023-03-18 11:05:25 -0600

    Made some progress on a calculator app.

---
+++ experiments/calculator/parenthesized_infix_calculator.c
@@ -0,0 +1,53 @@
+/* 
+ * A program that evaluates fully parenthesized 
+ * infix expressions using three stack data 
+ * structures.
+ *
+ * Author: C. Maxwell Reilly
+ * Date Completed:
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define MAX_SIZE 100 /* Maximum size is 100 chars. */
+
+int parren_top = -1, parren_array[MAX_SIZE];
+void parren_push();
+void parren_pop();
+void parren_show();
+
+int operand_top = -1, operand_array[MAX_SIZE];
+void operand_push();
+void operand_pop();
+void operand_show();
+
+
+int operator_top = -1, operator_array[MAX_SIZE];
+void operator_push();
+void operator_pop();
+void operator_show();
+
+
+int main ()
+{
+    return(0);
+}
+
+void parren_push()
+{
+    ;
+}
+
+void parren_pop()
+{
+    ;
+}
+
+void parren_show()
+{
+    ;
+}
+
+
