361c6d08ae49a21064c209a39630c04fc13af1d6
cmaxreilly <c.maxwell.reilly@gmail.com>
2023-05-24 13:47:37 -0600

    finished chapter 4 project 6

--- hanly_koffman/chapter_04_if_switch/programming_project_6/main.c
+++ hanly_koffman/chapter_04_if_switch/programming_project_6/main.c
@@ -7,22 +7,29 @@
 
 int day, month, year;
 
-int jan, feb, mar, apr, may, jun, jul, aug, sept, oct, nov, dec;
 
 int leap(int year);
 int
 main(void)
 {
+    int counter, month_days, total_days;
+    month_days = 0;
     printf("This program takes a date and tells you what day of the year\n");
     printf("it is.\n");
-    printf("Enter the day >> ");
+    printf("Enter the day in the format DD >> ");
     scanf("%d", &day);
-    printf("Enter the month >> ");
+    printf("Enter the month in the format MM>> ");
     scanf("%d", &month);
-    printf("Enter the year >> ");
+    printf("Enter the year in the format YYYY >> ");
     scanf("%d", &year);
     printf("\n");
-    leap(year);
+    int days_per_month[12] = {0, 31, 28 + leap(year), 31, 30, 31, 30, 31, 31, 30, 31, 30};
+    counter = month - 1;
+    while (counter >= 0){
+        month_days += days_per_month[counter];
+        counter -= 1;
+    }
+    printf("That day is number %d in the year.\n", month_days);
 
     return(0);
 
