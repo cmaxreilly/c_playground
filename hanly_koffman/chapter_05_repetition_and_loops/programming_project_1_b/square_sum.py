2e49d5326dad5cc57a93855b12c814344a62f8f3
cmaxreilly <c.maxwell.reilly@gmail.com>
2023-06-15 12:35:07 -0600

    Set up for the second problem

--- hanly_koffman/chapter_05_repetition_and_loops/programming_project_1_b/square_sum.py
+++
@@ -1,7 +0,0 @@
-numbers = [1,2,3,4,5]
-square_total = 0
-for number in numbers:
-    square_total += number ** 2
-
-print(str(square_total))
-
---
+++ hanly_koffman/chapter_05_repetition_and_loops/programming_project_2/main.c
@@ -0,0 +1,13 @@
+/*
+ * Author: C. Maxwell Reilly
+ * Date: Thursday, Jun 15, 2023
+ */
+
+#include <stdio.h>
+int
+main(void)
+{
+    return(0);
+
+}
+
