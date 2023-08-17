494e049d28e22071112a4a19ee1db11514700222
cmaxreilly <c.maxwell.reilly@gmail.com>
2023-08-17 00:15:36 -0600

    Made a new folder for algorithms

---
+++ clmr/insertion_sort/main.c
@@ -0,0 +1,52 @@
+/*
+ * Author: C. Maxwell Reilly
+ * Started: August 16, 2023
+ * Finished:
+ */
+
+
+#include <stdio.h>
+
+#define TEST 1
+
+// function prototypes
+int sort_array(int *array);
+
+int
+main(void)
+{
+    int array[6] = {5, 2, 4, 6, 1, 3};
+    // printf("test_array[0] = %d\n", sorted_array[0]);
+    int j, key, length, i;
+    length = sizeof(array)/ sizeof(array[0]);
+    printf("length of array = %d\n", length);
+    printf("Original array = {");
+    for (i = 0; i < length; i++)
+    {
+        printf("%d, ", array[i]);
+    }
+    printf(" }\n\nSorting ...\n\n");
+
+    for (   j = 1;
+            j <= length;
+            j ++)
+    {
+        key = array[j];
+        i = j - 1;
+        while (i > 0 && array[i] > key)
+        {
+            array[i + 1] = array[i];
+            i = i -1;
+        }
+        array[i + 1] = key;
+    }
+    printf("New array = { ");
+    for (i = 0; i < length; i++)
+    {
+        printf("%d, ", array[i]);
+    }
+    return 0;
+}
+
+
+
