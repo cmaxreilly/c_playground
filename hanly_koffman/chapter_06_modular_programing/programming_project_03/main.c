88ab014dc642988d500280296cc901145085ccf8
cmaxreilly <c.maxwell.reilly@gmail.com>
2023-10-28 15:52:40 -0600

    Started work on project 3

---
+++ hanly_koffman/chapter_06_modular_programing/programming_project_03/main.c
@@ -0,0 +1,19 @@
+/*
+ * Author: C. Maxwell Reilly
+ * Started: Saturday, October 28, 2023
+ * Finished:
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+/* Function prototypes */
+
+int
+main(void)
+{
+    /* Variables */
+
+    /* Driver code */
+    return 0;
+}
