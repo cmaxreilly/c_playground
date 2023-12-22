a81b4e67aa8f027fdc214d527a55e6c0b8d0be72
cmaxreilly <c.maxwell.reilly@gmail.com>
2023-12-21 18:53:02 -0700

    Work on init script

--- experiments/init_script/init_script_dir/init.sh
+++ experiments/init_script/init_script_dir/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-main () {
+function main {
     echo "This script will erase all of your work and initialize your project."
     echo "Are you sure that you want to do this? (type 'y' or 'n'):"
     read answer
@@ -15,9 +15,27 @@ main () {
 
 }
 
-erase_project() {
-    echo "Erasing source files"
-    cd src/ || echo "Error. No source directory. Aborting script"; exit
+function erase_project {
+    init_src
 }
 
+function init_src {
+    echo "Erasing source files"
+    echo -n > src/functions.c
+    echo '#include <stdio.h>' >> src/functions.c
+    echo '#include <math.h>' >> src/functions.c
+    echo '#include "functions.h"' >> src/functions.c
+}
+
+function erase_file_to_line {
+    local file=$1
+    local line=$2
+    if [ -f "$file" ]; then
+        # Truncate the file from the specified line
+        head -n 
+    fi
+}
+
+# Tests
+#erase_file_to_line ./init.sh 5
 main
--- experiments/init_script/restart.sh
+++ experiments/init_script/restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp init_script_dir/init.sh ./init.sh
 cp -r ./programming_project_03/ init_script_dir
 
 cp init.sh init_script_dir/init.sh
