e35b46c71014745493eb0a3dcf060fd620e22005
cmaxreilly <c.maxwell.reilly@gmail.com>
2024-02-02 19:09:34 -0700

    Work on HK ch 6 project 4

--- hanly_koffman/chapter_06_modular_programing/programming_project_04/src/main.c
+++ hanly_koffman/chapter_06_modular_programing/programming_project_04/src/main.c
@@ -16,5 +16,46 @@ main(void)
     printf("unknown quantity, enter a question mark (?).\n\n");
     printf("Rate of heat transfer (watts) >> ");
     scanf("%lf", &H);
+    printf("Coefficient of thermal conductivity (W/m-K) >> ");
+    scanf("%lf", &k);
+    printf("Cross-sectional are of conductor (m^2) >> ");
+    scanf("%lf", &A);
+    printf("Temperature on one side (K) >> ");
+    scanf("%lf", &T2);
+    printf("Temperature on other side (K) >> ");
+    scanf("%lf", &T1);
+    printf("Thickness of conductor (m) >> ");
+    scanf("%lf", &X);
+    printf("\n");
+    printf("            KA (T2 - T1)\n");
+    printf("H  =  ------------------------\n");
+    printf("                X\n\n");
+    if (H == 0)
+    {
+        printf("calc_h should be called\n");
+        return 0;
+    } else if (k == 0)
+    {
+        printf("calc_k should be called\n");
+        return 0;
+    } else if (A == 0)
+    {
+        printf("calc_A should be called\n");
+        return 0;
+    } else if (T2 == 0)
+    {
+        printf("calc_T2 should be called\n");
+        return 0;
+    } else if (T1 == 0)
+    {
+        printf("calc_T1 should be called\n");
+        return 0;
+    } else if (X == 0)
+    {
+        printf("calc_X should be called\n");
+        return 0;
+    }
+
     return 0;
+
 }
