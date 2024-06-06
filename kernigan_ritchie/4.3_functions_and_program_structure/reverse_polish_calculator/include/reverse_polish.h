7a89c53052035d15a9e95666e7ba43c3f5631252
cmaxreilly <c.maxwell.reilly@gmail.com>
2024-06-06 17:42:17 -0600

    Catching up

--- kernigan_ritchie/4.3_functions_and_program_structure/reverse_polish_calculator/include/reverse_polish.h
+++ kernigan_ritchie/4.3_functions_and_program_structure/reverse_polish_calculator/include/reverse_polish.h
@@ -1,5 +1,11 @@
+<<<<<<< HEAD
 #ifndef REVERSE_POLISH_CALCULATOR_H
 #define REVERSE_POLISH_CALCULATOR_H
+=======
+#ifndef REVERSE_POLISH_H
+#define REVERSE_POLISH_H
+
+>>>>>>> 2d87c6e (Catching up)
 #include <stdio.h>
 #include <stdlib.h>     /* for atof()   */
 #include <ctype.h>
@@ -8,6 +14,7 @@
 #define BUFSIZE 100     /* Buffer size for ungetch */
 #define MAXVAL  100     /* maxium depthc of the val stack */
 
+<<<<<<< HEAD
 /* Function Declarations */
 
 /* pop: pop and return top value from stack */
@@ -26,3 +33,24 @@ int getop(char s[]);
 void push(double f);
 
 #endif /* REVERSE_POLISH_CALCULATOR_H */
+=======
+/* Function declarations */
+
+    /* getop: get next operator or numeric operand */
+int getop(char s[]);
+    /* get a (possibly pushed back) character */
+int getch(void);
+     /* push character back on input */
+void ungetch(int c);
+    /* Pop a character from the stack */
+double pop(void);
+    /* push: push f onto value stack */
+void push(double f);
+    /* Figure out what is on the top of the stack */
+int getop(char s[]);
+
+
+
+
+#endif // REVERSE_POLISH_H
+>>>>>>> 2d87c6e (Catching up)
---
+++ kernigan_ritchie/4.3_functions_and_program_structure/reverse_polish_calculator/src/reverse_polish.c
@@ -0,0 +1,70 @@
+#include <include/reverse_polish.h>
+
+/* Global Variables */
+
+int sp = 0;         /* next free stack position */
+double val[MAXVAL]; /* value stack */
+char buf[BUFSIZE];  /* buffer for ungetch */
+int bufp = 0;
+
+void push(double f);
+/* pop: pop and return top value from stack */
+double
+pop(void)
+{
+    if (sp > 0)
+        return val[--sp];
+    else {
+        printf("error: stack empty\n");
+        return 0.0;
+    }
+}
+
+int
+getch(void) /* get a (possibly pushed back) character */
+{
+    return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void
+ungetch(int c)  /* push character back on input */
+{
+    if (bufp >= BUFSIZE)
+        printf("ungetch: too many characters\n");
+    else
+        buf[bufp++] = c;
+}
+
+int
+getop(char s[])
+{
+    int i, c;
+
+    while((s[0] = c = getch()) == ' ' || c == '\t')
+        ;
+    s[1] = '\0';
+    if (!isdigit(c) && c != '.')
+        return c;   /* not a number */
+    i = 0;
+    if (isdigit(c))
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    if (c == '.')   /* collect fraction part */
+        while (isdigit(s[++i] = c = getch()))
+            ;
+    s[i] = '\0';
+    if (c != EOF)
+        ungetch(c);
+    return NUMBER;
+}
+
+/* push: push f onto value stack */
+
+void
+push(double f)
+{
+    if (sp < MAXVAL)
+        val[sp++] = f;
+    else
+        printf("effor: stack full, can't push %g\n", f);
+}
