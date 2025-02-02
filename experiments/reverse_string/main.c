c0fe23677b4f2ff1501e6ee9df2240290cb37277
maxwell <Maxwell Reilly>
2025-02-02 13:35:33 -0700

    Finished utility that reverses strings.

--- experiments/reverse_string/main.c
+++ experiments/reverse_string/main.c
@@ -5,29 +5,25 @@
 
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
 
-char*
+void
 reverse_string(char* string_to_reverse)
 {
-    char* return_string;
-    int string_length = 0;
-    int length = 0;
+    char temp;
+    int length = strlen(string_to_reverse);
     int i1 = 0;
-    int i2;
+    int i2 = length -1;
 
-    while(string_to_reverse[length] != '\0')
-        length++;
-    // length += 1;
-    i2 = length - 1;
-    // printf("The first letter of your word is %c\n", string_to_reverse[0]);
-    // printf("The last letter of your word is %c\n", string_to_reverse[length - 1]);
-    while(i1 < length)
+    while(i1 < i2)
     {
-        return_string[i1] = string_to_reverse[i2];
+        temp = string_to_reverse[i1];
+        string_to_reverse[i1] = string_to_reverse[i2];
+        string_to_reverse[i2] = temp;
+
         i1++;
         i2--;
     }
-    return return_string;
 }
 
 int
@@ -37,6 +33,6 @@ main()
     printf("Enter a string you want reversed > ");
     scanf("%s", string);
     reverse_string(string);
-    printf("Your string is %s\n", string);
+    printf("Your string reversed is: %s\n", string);
     return 0;
 }
