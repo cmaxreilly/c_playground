2ffe537b167a99e3eb7e8f1d82bf04e7958c9593
maxwell <Maxwell Reilly>
2025-02-08 20:56:38 -0700

    Working on raylib/working on go

---
+++ games/raylib_window/main.c
@@ -0,0 +1,10 @@
+// #include "raylib.h"
+
+int
+main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    return 0;
+}
