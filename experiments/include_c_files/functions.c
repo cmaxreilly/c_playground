1e8dec0648bb8b3b1da40fbdc5cc5b882ed09eb3
maxwell <Maxwell Reilly>
2025-03-17 20:54:42 -0600

    Added include files

---
+++ experiments/include_c_files/functions.c
@@ -0,0 +1,5 @@
+void
+hello(void)
+{
+    printf("Hello from functions.c!\n");
+}
---
+++ experiments/include_c_files/main.c
@@ -0,0 +1,9 @@
+# include <stdio.h>
+# include "functions.c"
+
+int main(void)
+{
+    printf("Hello from main!\n");
+    hello();
+    return 0;
+}
