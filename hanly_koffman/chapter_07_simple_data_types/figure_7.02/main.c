108c76ff5ba497569bf1357308adb6eaa29d7f33
maxwell <Maxwell Reilly>
2025-03-21 01:22:14 -0600

    Added some data types

--- hanly_koffman/chapter_07_simple_data_types/figure_7.02/main.c
+++ hanly_koffman/chapter_07_simple_data_types/figure_7.02/main.c
@@ -10,7 +10,13 @@ int
 main(void)
 {
     printf("Range of positive values of type int: 1 . . %d\n", INT_MAX);
+    printf("This is equivalent to %d bits, or %zu bytes.\n\n", 8 * (int)sizeof(int), sizeof(int));
     printf("Range of positive values of type double: %e . . %e\n", DBL_MIN, DBL_MAX);
+    printf("This is equivalent to %d bits, or %zu bytes.\n\n", 8 * (int)sizeof(double), sizeof(double));
+    printf("Range of positive values of type long: %ld . . %ld\n", LONG_MIN, LONG_MAX);
+    printf("This is equivalent to %d bits, or %zu bytes.\n\n", 8 * (int)sizeof(long), sizeof(long));
+    printf("Range of positive values of type long long: %lld . . %lld\n", LLONG_MIN, LLONG_MAX);
+    printf("This is equivalent to %d bits, or %zu bytes.\n\n", 8 * (int)sizeof(long long), sizeof(long long));
 
     return(0);
 }
