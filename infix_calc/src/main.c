cbf31d0cf3a241539ac731920440a8362f2c4888
maxwell <Maxwell Reilly>
2025-03-28 21:47:02 -0600

    Wrote getch and buffer from scratch.

--- infix_calc/src/main.c
+++ infix_calc/src/main.c
@@ -1,10 +1,31 @@
-#include <stdio.h>
+/* Libraries */
+#include <stdio.h> /* For printf() */
 #include <ctype.h>
 #include <stdlib.h>
 
+#define NUMBER 0
+#define BUFMAX 1000
+int getch_buf[BUFMAX]; /* For ungetch */
+int bufp = 0; /* pointer for buffer */
+
+int getch(void);
 int
 main(void)
 {
     printf("\nHello, world!\n\n");
     return 0;
+}
+
+int
+getch(void)
+{
+    if (bufp > 0)
+    {
+        bufp--;
+        return getch_buf[bufp];
+    }
+    else
+    {
+        return getchar();
+    }
 }
