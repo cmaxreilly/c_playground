23574c802742ccaf5fc19214315fd0d158d2931f
C. Maxwell Reilly <c.maxwell.reilly@gmail.com>
2025-08-03 11:04:20 -0600

    Named variables in find roots function

--- hanly_koffman/chapter_07_simple_data_types/programming_project_05/main.c
+++ hanly_koffman/chapter_07_simple_data_types/programming_project_05/main.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
 #include<math.h> /* For frac */
 
+double
+find_root(double radicand, double index);
+
 int
 main(void)
 {
+    find_root(4, 2);
     return 0;
 }
+
+double
+find_root(double radicand, double index)
+{
+    double answer = 0;
+    return answer;
+}
+
