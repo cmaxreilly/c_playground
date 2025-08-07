5e461d5ba2dcd524465350d77b89842025d512f3
C. Maxwell Reilly <c.maxwell.reilly@gmail.com>
2025-08-07 13:55:01 -0600

    Finished Programming Project 5

--- hanly_koffman/chapter_07_simple_data_types/programming_project_05/main.c
+++ hanly_koffman/chapter_07_simple_data_types/programming_project_05/main.c
@@ -8,12 +8,21 @@ int
 main(void)
 {
     printf("\nsquare root of 4 is %lf\n", find_root(4, 2));
+    printf("\nThe square root of 2 is %lf\n", find_root(2, 2));
+    printf("\nThe cube root of 7 is %lf\n", find_root(7, 3));
+    printf("\nThe cube root of -1 is %lf\n", find_root(-1, 3));
     return 0;
 }
 
 double
 find_root(double radicand, double index)
 {
+    if (radicand <= 0)
+    {
+        printf("\nNegative radicands are illegal\n");
+        return 0.0;
+    }
+
     double guess = radicand / 2.0;
     double answer = 0;
     for (
@@ -21,7 +30,7 @@ find_root(double radicand, double index)
             i < 100;
             i++)
     {
-        guess = guess - (pow(guess, index) - radicand/index * guess);
+        guess = guess - (pow(guess, index) - radicand)/ (index * pow(guess, index - 1.0));
     }
     answer = guess;
     return answer;
