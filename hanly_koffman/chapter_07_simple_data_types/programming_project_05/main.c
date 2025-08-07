0c860edb0a1e237caf132e5fa279b55386603583
C. Maxwell Reilly <c.maxwell.reilly@gmail.com>
2025-08-06 22:51:44 -0600

    First draft of find root function complete

--- hanly_koffman/chapter_07_simple_data_types/programming_project_05/main.c
+++ hanly_koffman/chapter_07_simple_data_types/programming_project_05/main.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<math.h> /* For frac */
+#include<math.h> /* For pow*/
 
 double
 find_root(double radicand, double index);
@@ -7,14 +7,23 @@ find_root(double radicand, double index);
 int
 main(void)
 {
-    find_root(4, 2);
+    printf("\nsquare root of 4 is %lf\n", find_root(4, 2));
     return 0;
 }
 
 double
 find_root(double radicand, double index)
 {
+    double guess = radicand / 2.0;
     double answer = 0;
+    for (
+            int i = 0;
+            i < 100;
+            i++)
+    {
+        guess = guess - (pow(guess, index) - radicand/index * guess);
+    }
+    answer = guess;
     return answer;
 }
 
