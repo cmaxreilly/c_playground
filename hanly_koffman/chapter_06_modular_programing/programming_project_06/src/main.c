c21ac66f7d1285896620fe9688a7af93ca9f85cf
Max Reilly <c.maxwell.reilly@gmail.com>
2026-04-22 12:41:53 -0600

    Initialized programming project no 6

---
+++ hanly_koffman/chapter_06_modular_programing/programming_project_06/src/main.c
@@ -0,0 +1,15 @@
+/*
+ * Author: C. Maxwell Reilly
+ * Work started: 2026.04.22
+ */
+
+#include <stdio.h>
+
+int
+main(void)
+{
+    int customer_num = 0;
+    int total_kwh = 0;
+    float total_charges = 0;
+    return 0;
+}
