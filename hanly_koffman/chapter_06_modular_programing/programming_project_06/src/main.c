380c5259a10478187748f5b8485d1752c5919be2
Max Reilly <c.maxwell.reilly@gmail.com>
2026-04-22 14:57:05 -0600

    Finished main function

--- hanly_koffman/chapter_06_modular_programing/programming_project_06/src/main.c
+++ hanly_koffman/chapter_06_modular_programing/programming_project_06/src/main.c
@@ -8,8 +8,26 @@
 int
 main(void)
 {
-    int customer_num = 0;
+    int num_of_customers = 0;
     int total_kwh = 0;
     float total_charges = 0;
+    FILE *data;
+    int customer_number;
+    int customer_usage;
+    int label_number = 1;
+
+    data = fopen("dat/customer_kilowatt-hours.dat", "r");
+    fscanf(data, "%d", &customer_number);
+    fscanf(data, "%d", &customer_usage);
+    printf("%d: %d, usage: %d\n", label_number, customer_number, customer_usage);
+    while (fscanf(data, "%d", &customer_number) == 1)
+    {
+        fscanf(data, "%d", &customer_usage);
+        label_number++;
+        printf("%d: customer %d: %d kwh\n", label_number, customer_number, customer_usage);
+    }
+
+
+    fclose(data);
     return 0;
 }
