284e63a6ee1c7332d2ae5166e0039a812e225d0d
Max Reilly <c.maxwell.reilly@gmail.com>
2026-04-23 16:06:03 -0600

    Finished programming project no 9

--- hanly_koffman/chapter_06_modular_programing/programming_project_09/src/main.c
+++ hanly_koffman/chapter_06_modular_programing/programming_project_09/src/main.c
@@ -6,8 +6,27 @@
 #include <stdio.h>
 #include <math.h>
 
+void print_table(int start, int end);
+
 int
 main(void)
 {
+    int start, end;
+    printf("This program scans 2 values and then creates a table of negative powers of two.\n");
+    printf("enter start value > ");
+    scanf("%d", &start);
+    printf("enter end value > ");
+    scanf("%d", &end);
+    printf("Power of 2 | Fraction | Decimal value\n");
+    print_table(start, end);
     return 0;
+}
+
+void
+print_table(int start, int end)
+{
+    for (int i = start; i <= end ; i++)
+    {
+        printf("-%4d      | 1/%2.0f     | %.4f\n", i, pow(2, i), pow(2.0, (double) i * -1));
+    }
 }
