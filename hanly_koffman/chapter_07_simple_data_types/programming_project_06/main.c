4c203978f3bbd6d50f029b7f87464f3a6eccd5af
Max Reilly <c.maxwell.reilly@gmail.com>
2026-04-26 19:29:53 -0600

    finished trapezoid method function for pp6

--- hanly_koffman/chapter_07_simple_data_types/programming_project_06/main.c
+++ hanly_koffman/chapter_07_simple_data_types/programming_project_06/main.c
@@ -2,6 +2,8 @@
 #include<math.h> /* for sin and sqrt */
 
 double trap(double a, double b, double n, double (*f) (double) );
+/* a is start of interval, b is end of interval, n is divisions of interval,
+ * f is function. */
 double sine_func(double n);
 double g(double x);
 double f(double x);
@@ -9,25 +11,37 @@ double f(double x);
 int
 main(void)
 {
-    printf("\nSine of 9 is %f\n", sine_func(9.00));
-    printf("This number should be the same: %f\n", trap(1.0, 1.0, 9.0, sine_func));
+    // printf("\nSine of 3.1415 is %f\n", sine_func(3.1415));
+    // printf("Should be = 0.00\n");
+    printf("Definite integral of sin(0, 3.1415) = 2\n");
+    printf("This number should be the same: %f\n", trap(0, 3.1415, 9.0, sine_func));
     return 0;
 }
 
 double
-trap(double a, double b, double n, double (*f) (double) )
+trap(double a, double b, double n, double f(double farg) )
 {
-    double h = (b - a ) / n;
-    double sum = 0;
+    double h = (b - a ) / n; // Length of segments
+    // printf("a = %lf\n", a);
+    // printf("b = %lf\n", b);
+    // printf("n = %lf\n", n);
+    // printf("h = (b - a) / n = %lf (should equal \n", h);
+    double sum = 0.0, line_one, line_two, segment_area;
     int i;
 
-    for (   i = 1;
+    for (   i = 0;
             i < n;
             i++)
     {
-        sum += f(i/n);
+        line_one = f(a + (i * h));
+        line_two = f(a + ((i + 1) * h));
+        segment_area = ((line_one + line_two) / 2) * h;
+        // printf("line one length = %lf\n", line_one);
+        // printf("line two length = %lf\n", line_two);
+        // printf("segment %d area = %lf\n\n", i, segment_area);
+        sum += segment_area;
     }
-    return (h / 2) * (f(a) + f(b) + sum);
+    return sum;
 }
 
 double
